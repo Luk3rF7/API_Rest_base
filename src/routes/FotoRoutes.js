e213ee6968b52bd7226db67a847a378eed4e8d57
luk3rF7 <lkzeron@gmail.com>
2023-06-18 07:54:08 -0300

    versao 0 de sistema de routes

---
+++ src/routes/FotoRoutes.js
@@ -0,0 +1,9 @@
+// === import primmeiros ===
+import { Router } from 'express';
+// === import modules locais depois dos imports principais ===
+import loginRequired from '../middlewares/loginRequired';
+import fotoController from '../controller/FotoController';
+
+const router = new Router();
+router.post('/', loginRequired, fotoController.store);
+export default router;
---
+++ src/routes/TokenRoutes.js
@@ -0,0 +1,12 @@
+// === imports ===
+import { Router } from 'express';
+import TokenController from '../controller/TokenController';
+
+//  === routes & settings ===
+
+const router = new Router();
+
+// ==== config routes ===
+router.post('/', TokenController.store);
+
+export default router;
